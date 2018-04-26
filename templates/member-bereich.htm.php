1c4bab7af5d5250e580783a2001952340b1b596a
javi36 <jmartinezalvarez0@gmail.com>
2018-04-26 14:06:19 +0200

    Fotogalerie Modal erstellt

--- templates/member-bereich.htm.php
+++ templates/member-bereich.htm.php
@@ -7,8 +7,34 @@
  */
 
 
-
 ?>
+<button class="btn btn-primary" data-toggle="modal" data-target="#erstellen">Fotogalerie erstellen</button>
+
 <div class="col-md-12">
    <h3>Willkommen  </h3>
+</div>
+
+
+<div class="modal fade" id="erstellen" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Fotogalerie erstellen</h4>
+            </div>
+            <div class="modal-body">
+                <span class="label-input100"  for="galerieName">Name *</span>
+                <input class="input100" type="text" id="galerieName" name="galerieName"  >
+                <br>
+                <span class="label-input100"  for="galerieBeschreibung">Beschreibung</span>
+                <input class="input100" type="text" id="galerieBeschreibung" name="galerieBeschreibung"  >
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success">Erstellen</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Abbrechen</button>
+            </div>
+        </div>
+
+    </div>
 </div>
