d84b12b110063faf6c9b0f945d04486865ec93a9
princess3112 <deck.irina99@gmail.com>
2018-05-17 14:29:48 +0200

    Erstellen

--- templates/galerieErstellen.htm.php
+++ templates/galerieErstellen.htm.php
@@ -8,13 +8,38 @@
 
 
 ?>
-<form class="" name="" action="" method="post">
-    <span class="label-input100" for="galerieName">Name *</span>
-    <input class="input100" type="text" id="galerieName" name="galerieName">
-    <br>
-    <span class="label-input100" for="galerieBeschreibung">Beschreibung</span>
-    <input class="input100" type="text" id="galerieBeschreibung" name="galerieBeschreibung">
-    <button type="submit" class="btn btn-success">Erstellen</button>
-    <p style="color: rgb(150,86,242);"><?php echo getValue("RegiError")?></p>
-</form>
+<link rel="stylesheet" type="text/css" href="../css/galerieErstellen.css">
+<div class="limiter">
+    <div class="container-fluid container-login100" >
+        <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54" ">
+        <form class="login100-form validate-form" name="kontakt" action="" method="post">
+            <div style="margin-bottom: 60px;">
+					<span class="login100-form-title p-b-49" style="font-size: 50px; color: rgb(150,86,242); font-weight: bold; ">
+						Galerie erstellen
+					</span>
 
+                <p style="color: rgb(150,86,242);"><?php echo getValue("RegiError")?></p>
+            </div>
+            <div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired" style="padding-bottom: 40px;" >
+                <span class="label-input100" style="font-size: 20px; color: rgb(118,131,239); font-weight: bold;  " for="galerieName">Name *</span>
+                <input class="input100" type="text" id="galerieName" name="galerieName"  style="border-bottom: 1px solid #ccc !important;">
+                <span class="focus-input100" data-symbol="&#xf206;"></span>
+            </div>
+
+            <div class="wrap-input100 validate-input" data-validate="Password is required" style="padding-bottom: 40px;">
+                <span class="label-input100"  style="font-size: 20px; color: rgb(118,131,239); font-weight: bold;" for="galerieBeschreibung">Beschreibung</span>
+                <input class="input100" type="text" id="galerieBeschreibung" name="galerieBeschreibung" style= "border-bottom: 1px solid #ccc !important;" >
+                <span class="focus-input100" data-symbol="&#xf190;"></span>
+            </div>
+
+            <div class="container-login100-form-btn">
+                <div class="wrap-login100-form-btn">
+                    <div class="login100-form-bgbtn"></div>
+                    <button type="submit" class="login100-form-btn" name="login" id="login">
+                        Erstellen
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
